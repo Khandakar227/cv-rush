94bd1b28164f81f9deea7052bc9fa34553e0ae2e
Khandakar227 <xenonknight54@gmail.com>
2023-05-29 01:04:41 +0600

    Notification system

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,43 @@ import {
 } from "react-router-dom";
 import Home from './pages/home';
 import CVgenerator from "./pages/cv-generator";
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useEffect } from "react";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home/>,
+    element: <Home />,
   },
   {
     path: "/cvgenerator",
-    element: <CVgenerator/>,
+    element: <CVgenerator />,
   },
 ]);
 
 function App() {
-
+  useEffect(() => {
+    const userAgent = navigator.userAgent.toLowerCase();
+    const Android = userAgent.indexOf("android") > -1;
+    
+    if (Android || window.innerWidth <= 600) {
+      toast.warn('Note: This app was designed for desktop users. It is not mobile responsive.');
+    }
+  }, [])
   return (
     <>
-    <RouterProvider router={router} />
+      <RouterProvider router={router} />
+      <ToastContainer
+        position="top-center"
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        limit={3}
+        draggable
+        theme="colored"
+      />
     </>
   )
 }
